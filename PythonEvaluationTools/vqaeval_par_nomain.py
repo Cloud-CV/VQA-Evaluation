1df5a4da01baa94c7cc33116c7b77456da3bfe66
prithv1 <prithvijitchattopadhyay@gmail.com>
2016-12-22 03:21:58 -0500

    Removed sub-process dependence

---
+++ PythonEvaluationTools/vqaeval_par_nomain.py
@@ -0,0 +1,87 @@
+
+"""
+Input: GT json files, Pred json files
+
+"""
+# coding: utf-8
+import multiprocessing
+import sys
+dataDir = '../../VQA-Evaluation'   #Change this according to the repository name
+sys.path.insert(0, '%s/PythonHelperTools/vqaTools' %(dataDir))
+from vqa import VQA
+from vqaEvaluation.vqaEval import VQAEval
+import os
+import time
+import numpy as np
+
+def prepare_objects(annFile, quesFile, resFile, chunk):
+	global CHUNK_SZ 
+	CHUNK_SZ = chunk
+	global vqa 
+	vqa = VQA(annFile, quesFile)
+	global vqaRes
+	vqaRes = vqa.loadRes(resFile, quesFile)
+	global vqaEval
+	vqaEval = VQAEval(vqa, vqaRes, n=2)
+	
+"""
+Slightly more optimized implementation of splitting stuff
+Saves ~2 seconds
+Flipped the process of computing question-type accuracies. Good Stuff, the chunking idea!
+"""
+def get_iter_arr(length_qids):
+	factor = int(length_qids/CHUNK_SZ)
+	remainder = length_qids % CHUNK_SZ
+	len_array = np.ones(CHUNK_SZ)
+	len_array = factor*len_array
+	if remainder != 0:
+		len_array[-1] = remainder
+	return len_array.tolist()
+
+def vqaeval(qid_list):
+	vqaEval.evaluate(qid_list.tolist())
+	return vqaEval.accuracy['overall']
+
+def reduce_acc(results_list, length_list, length):
+	return float(sum([a*b for a,b in zip(results_list, length_list)])) / length
+"""
+End 
+"""
+
+def Evaluate(annFile, quesFile, resFile, chunk_size):
+	prepare_objects(annFile, quesFile, resFile, chunk_size)
+	all_qids = vqa.getQuesIds()
+	binary_qids = vqa.getQuesIds(ansTypes='yes/no')
+	number_qids = vqa.getQuesIds(ansTypes='number')	
+	other_qids = vqa.getQuesIds(ansTypes='other')
+
+	t = time.time()
+	pool = multiprocessing.Pool(12)
+	
+	## Binary Accuracies
+	binary_qids_split = np.array_split(binary_qids, CHUNK_SZ)
+	binary_qids_len = get_iter_arr(len(binary_qids))
+	binary_results = pool.map(vqaeval, binary_qids_split)
+	binary_acc = reduce_acc(binary_results, binary_qids_len, len(binary_qids))
+	print(binary_acc)
+
+	## Number Accuracies
+	number_qids_split = np.array_split(number_qids, CHUNK_SZ)
+	number_qids_len = get_iter_arr(len(number_qids))
+	number_results = pool.map(vqaeval, number_qids_split)
+	number_acc = reduce_acc(number_results, number_qids_len, len(number_qids))
+	print(number_acc)
+
+	## Other Accuracies
+	other_qids_split = np.array_split(other_qids, CHUNK_SZ)
+	other_qids_len = get_iter_arr(len(other_qids))
+	other_results = pool.map(vqaeval, other_qids_split)
+	other_acc = reduce_acc(other_results, other_qids_len, len(other_qids))
+	print(other_acc)
+
+	## Overall Accuracy
+	overall_acc = float(float(other_acc*len(other_qids)) + float(number_acc*len(number_qids)) + float(binary_acc*len(binary_qids))) / len(all_qids)
+	print(overall_acc)
+
+	elapsed = time.time() - t
+	print "Elapsed Time: " + str(elapsed)	
--- PythonEvaluationTools/vqaeval_receiver.py
+++ PythonEvaluationTools/vqaeval_receiver.py
@@ -2,13 +2,10 @@ from subprocess import call
 import pika
 import multiprocessing
 import sys
-dataDir = '../../VQA'
-sys.path.insert(0, '%s/PythonHelperTools/vqaTools' %(dataDir))
-from vqa import VQA
-from vqaEvaluation.vqaEval import VQAEval
 import os
 import time
 import yaml
+import vqaeval_par_nomain as eval_fun
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
@@ -19,8 +16,10 @@ vqa_dict = {}
 def callback(ch, method, properties, body):
 	print(" [x] Received %r" % body)
 	vqa_dict = yaml.safe_load(body)
-	arg_string = 'python vqaeval_par.py ' + vqa_dict['anno'] + ' ' + vqa_dict['ques'] + ' ' + vqa_dict['pred']  
-	os.system(arg_string)
+	eval_fun.Evaluate(vqa_dict['anno'], vqa_dict['ques'], vqa_dict['pred'], 16)
+	# Not required now
+	# arg_string = 'python vqaeval_par.py ' + vqa_dict['anno'] + ' ' + vqa_dict['ques'] + ' ' + vqa_dict['pred']  
+	# os.system(arg_string)
 
 channel.basic_consume(callback,queue='hello',no_ack=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
