d1e934c9bbfa11945597d38e4970f7b70dba9e0c
prithv1 <prithvijitchattopadhyay@gmail.com>
2016-12-24 01:42:14 -0500

    minor changes in the travis script and bash script

--- Data/download_anno.sh
+++ Data/download_anno.sh
@@ -6,5 +6,6 @@
 # Distributed under terms of the MIT license.
 #
 
-
+cd Data
 wget https://filebox.ece.vt.edu/~prithv1/vqa_jsons.tar.gz && tar -xvzf vqa_jsons.tar.gz && rm -rf vqa_jsons.tar.gz && cp $HOME/Cloud-CV/VQA-Evaluation/Results/OpenEnded_mscoco_train2014_fake_results.json VQA_jsons/
+cd ..
